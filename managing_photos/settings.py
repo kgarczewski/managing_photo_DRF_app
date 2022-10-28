c01c7b7b9a6d7760b585dfc416221e9f3cb44075
k.garczewski <garczewskikrzysztof@gmail.com>
2022-10-28 21:43:32 +0200

    adding gitignore

--- managing_photos/settings.py
+++ managing_photos/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m_^^8*x)-8394b^8n)%u9#164w$$1(f_wl2if^7j7yum@1fqab'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
